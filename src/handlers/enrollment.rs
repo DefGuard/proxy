67d5d064cdd00a564fb2a1b522c67ebc408f6a07
Maciej Wójcik <wojcik91@gmail.com>
2023-07-26 21:50:04 +0200

    layout enrollment routes

---
+++ src/handlers/enrollment.rs
@@ -0,0 +1,11 @@
+pub async fn start_enrollment_process() {
+    unimplemented!()
+}
+
+pub async fn activate_user() {
+    unimplemented!()
+}
+
+pub async fn create_device() {
+    unimplemented!()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
-use axum::{routing::get, Router};
+use axum::{routing::{get, post}, Router};
 use tower_http::trace::{self, TraceLayer};
 use tracing::{info, Level};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+use defguard_proxy::handlers::enrollment;
 
 #[tokio::main]
 async fn main() {
     // initialize tracing
     tracing_subscriber::registry()
         .with(
-            tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| "info".into()),
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| "info".into()),
         )
         .with(tracing_subscriber::fmt::layer())
         .init();
@@ -17,6 +18,16 @@ async fn main() {
     // build application
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
+        .nest(
+            "/api/v1",
+            Router::new().nest(
+                "/enrollment",
+                Router::new()
+                    .route("/start", post(enrollment::start_enrollment_process))
+                    .route("/activate_user", post(enrollment::activate_user))
+                    .route("/create_device", post(enrollment::create_device)),
+            ),
+        )
         .layer(
             TraceLayer::new_for_http()
                 .make_span_with(trace::DefaultMakeSpan::new().level(Level::INFO))
@@ -25,7 +36,7 @@ async fn main() {
 
     // run server
     info!("Starting server");
-    axum::Server::bind(&"0.0.0.0:3001".parse().unwrap())
+    axum::Server::bind(&"0.0.0.0:8000".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
