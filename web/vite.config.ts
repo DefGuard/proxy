8c79e842a56169c480bbc38b7984bcedee51f1ee
Maciej Wójcik <wojcik91@gmail.com>
2023-08-03 08:38:07 +0200

    update proxy port

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8000/',
+        target: 'http://127.0.0.1:8080/',
         changeOrigin: true,
       },
     },
