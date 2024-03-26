b552df71be832cd623bc4bd69229e3f44e9d60e7
Jacek Chmielewski <jchmielewski@teonite.com>
2024-03-26 11:09:44 +0100

    Managed to format date

--- src/http_tracing_formatter.rs
+++ src/http_tracing_formatter.rs
@@ -2,21 +2,64 @@ use rust_tracing::{Event, Subscriber};
 use std::fmt;
 use tracing_subscriber::{
     fmt::{
-        format::{self, FormatEvent, FormatFields},
-        FmtContext, FormattedFields,
+        format::{self, FormatEvent, FormatFields, Writer},
+        FmtContext, FormattedFields, time::{SystemTime, FormatTime},
     },
     registry::LookupSpan,
 };
 
-pub(crate) struct HttpFormatter<E = format::Format<format::Full>> {
-    inner: E,
+#[derive(Default)]
+pub(crate) struct HttpFormatter<F = format::Format<format::Full>> {
+    inner: F,
+    timer: SystemTime,
 }
 
-impl Default for HttpFormatter {
-    fn default() -> Self {
-        HttpFormatter {
-            inner: format::Format::default(),
+// impl Default for HttpFormatter {
+//     fn default() -> Self {
+//         HttpFormatter {
+//             inner: format::Format::default(),
+//             timer: SystemTime::default(),
+//         }
+//     }
+// }
+
+impl HttpFormatter {
+
+    fn format_timestamp(&self, writer: &mut Writer<'_>) -> fmt::Result
+    // where
+    //     T: FormatTime,
+    {
+        // // If timestamps are disabled, do nothing.
+        // if !self.display_timestamp {
+        //     return Ok(());
+        // }
+
+        // // If ANSI color codes are enabled, format the timestamp with ANSI
+        // // colors.
+        // #[cfg(feature = "ansi")]
+        // {
+        //     if writer.has_ansi_escapes() {
+        //         // let style = Style::new().dimmed();
+        //         // write!(writer, "{}", style.prefix())?;
+
+        //         // If getting the timestamp failed, don't bail --- only bail on
+        //         // formatting errors.
+        //         if self.timer.format_time(writer).is_err() {
+        //             writer.write_str("<unknown time>")?;
+        //         }
+
+        //         write!(writer, "{} ", style.suffix())?;
+        //         return Ok(());
+        //     }
+        // }
+
+        // Otherwise, just format the timestamp without ANSI formatting.
+        // If getting the timestamp failed, don't bail --- only bail on
+        // formatting errors.
+        if self.timer.format_time(writer).is_err() {
+            writer.write_str("<unknown time>")?;
         }
+        writer.write_char(' ')
     }
 }
 
@@ -33,12 +76,12 @@ where
     ) -> fmt::Result {
         // Format values from the event's's metadata:
         let metadata = event.metadata();
+        self.format_timestamp(&mut writer);
         write!(&mut writer, "HTTP_FORMATTER");
         // write!(&mut writer, "{} {}: ", metadata.level(), metadata.target())?;
 
         // Format all the spans in the event's span context.
         let sc = ctx.clone();
-        sc.sc
         self.inner.format_event(ctx, writer, event);
         // if let Some(scope) = ctx.event_scope() {
         //     for span in scope.from_root() {
