5f4e17b152356ef111b823ac7f679b57c3d6f323
Cyprian <46838495+cpprian@users.noreply.github.com>
2024-08-13 16:48:23 +0200

    add debug logs (#75)
    * add debug logs to better understand flow between core - proxy - client
    
    * Apply suggestions from code review
    
    Co-authored-by: Aleksander <170264518+t-aleksander@users.noreply.github.com>
    ---------
    
    Co-authored-by: Aleksander <170264518+t-aleksander@users.noreply.github.com>

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -46,9 +46,14 @@ where
 ///
 /// Waits for core response with a given timeout and returns the response payload.
 async fn get_core_response(rx: Receiver<Payload>) -> Result<Payload, ApiError> {
+    debug!("Fetching core response...");
     if let Ok(core_response) = timeout(Duration::from_secs(CORE_RESPONSE_TIMEOUT), rx).await {
         debug!("Got gRPC response from Defguard core: {core_response:?}");
         if let Ok(Payload::CoreError(core_error)) = core_response {
+            error!(
+                "Received an error response from the core service. | status code: {} message: {}",
+                core_error.status_code, core_error.message
+            );
             return Err(core_error.into());
         };
         core_response
