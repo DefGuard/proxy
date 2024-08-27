12953faf75ef693aae4ee720f9e64da9bbfb9ab4
Jacek Chmielewski <jchmielewski@teonite.com>
2024-08-27 13:11:49 +0200

    wip: info handler

--- src/handlers/info.rs
+++ src/handlers/info.rs
@@ -12,33 +12,33 @@ use crate::{
     },
 };
 
-pub fn router() -> Router<AppState> {
-    Router::new()
-        .route("/info", get(location_info))
-}
+// pub fn router() -> Router<AppState> {
+//     Router::new()
+//         .route("/info", get(location_info))
+// }
 
-#[instrument(level = "debug", skip(state))]
-pub async fn location_info(
-    State(state): State<AppState>,
-    private_cookies: PrivateCookieJar,
-) -> Result<Json<DeviceConfigResponse>, ApiError> {
-    // let pubkey = req.pubkey.clone();
-    info!("Getting network info for device {pubkey}");
+// #[instrument(level = "debug", skip(state))]
+// pub async fn location_info(
+//     State(state): State<AppState>,
+//     private_cookies: PrivateCookieJar,
+// ) -> Result<Json<DeviceConfigResponse>, ApiError> {
+//     // let pubkey = req.pubkey.clone();
+//     info!("Getting network info for device {pubkey}");
 
-    // set auth info
-    req.token = private_cookies
-        .get(ENROLLMENT_COOKIE_NAME)
-        .map(|cookie| cookie.value().to_string());
+//     // set auth info
+//     req.token = private_cookies
+//         .get(ENROLLMENT_COOKIE_NAME)
+//         .map(|cookie| cookie.value().to_string());
 
-    let rx = state
-        .grpc_server
-        .send(Some(core_request::Payload::ExistingDevice(req)), None)?;
-    let payload = get_core_response(rx).await?;
-    if let core_response::Payload::DeviceConfig(response) = payload {
-        info!("Got network info for device {pubkey}");
-        Ok(Json(response))
-    } else {
-        error!("Received invalid gRPC response type: {payload:#?}");
-        Err(ApiError::InvalidResponseType)
-    }
-}
+//     let rx = state
+//         .grpc_server
+//         .send(Some(core_request::Payload::ExistingDevice(req)), None)?;
+//     let payload = get_core_response(rx).await?;
+//     if let core_response::Payload::DeviceConfig(response) = payload {
+//         info!("Got network info for device {pubkey}");
+//         Ok(Json(response))
+//     } else {
+//         error!("Received invalid gRPC response type: {payload:#?}");
+//         Err(ApiError::InvalidResponseType)
+//     }
+// }
