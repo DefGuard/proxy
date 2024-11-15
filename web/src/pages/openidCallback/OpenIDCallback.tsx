ecb90926d204cc475d4e8202997c39620843abfe
Aleksander <170264518+t-aleksander@users.noreply.github.com>
2024-11-15 18:23:56 +0100

    cleanup

--- web/src/pages/openidCallback/OpenIDCallback.tsx
+++ web/src/pages/openidCallback/OpenIDCallback.tsx
@@ -3,7 +3,6 @@ import './style.scss';
 import { LogoContainer } from '../../components/LogoContainer/LogoContainer';
 import { PageContainer } from '../../shared/components/layout/PageContainer/PageContainer';
 import { OpenIDCallbackCard } from './components/OpenIDCallbackCard';
-// import { TokenCard } from './components/TokenCard';
 
 export const OpenIDCallbackPage = () => {
   return (
