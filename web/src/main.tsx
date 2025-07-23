b606d79310372a6c28b39781125fb6423f85e05e
Filip Ślęzak <fslezak@teonite.com>
2025-07-23 13:24:26 +0200

    remove old scss helpers, fix font imports

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,6 +1,8 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import './shared/defguard-ui/scss/index.scss';
+import './shared/scss/index.scss';
 
 import { App } from './components/App/App';
 
