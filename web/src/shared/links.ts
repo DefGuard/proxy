78fe26115a5b4d0f887102a68f0a4ee5e8902daa
Filip Ślęzak <102536422+filipslezaklab@users.noreply.github.com>
2025-09-11 12:16:01 +0200

    update mobile app apple store link (#161)

--- web/src/shared/links.ts
+++ web/src/shared/links.ts
@@ -2,7 +2,7 @@ export const sharedLinks = {
   client: {
     download: {
       android: 'https://play.google.com/store/apps/details?id=net.defguard.mobile',
-      ios: 'https://testflight.apple.com/join/Jvdhkt7h',
+      ios: 'https://apps.apple.com/us/app/defguard-vpn-client/id6748068630',
       desktop: 'https://defguard.net/download/',
     },
   },
