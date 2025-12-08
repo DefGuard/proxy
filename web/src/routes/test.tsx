2e0fc69bdc7047f184afc85bb09df759ce3bb957
Filip Ślęzak <fslezak@teonite.com>
2025-12-08 15:16:21 +0100

    add link invalid page

--- web/src/routes/test.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router';
-import { TestPage } from '../test_components/page/TestPage';
-
-export const Route = createFileRoute('/test')({
-  component: TestPage,
-});
