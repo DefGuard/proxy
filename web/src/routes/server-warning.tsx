3693e955467af70e2843bb65da921aebed5b652e
Jacek Chmielewski <jacek@defguard.net>
2026-03-02 12:53:25 +0100

    move server warning page to separate component

--- web/src/routes/server-warning.tsx
+++ web/src/routes/server-warning.tsx
@@ -1,35 +1,11 @@
 import { createFileRoute } from '@tanstack/react-router';
-import { m } from '../paraglide/messages';
+import { ServerWarningPage } from '../pages/ServerWarning/ServerWarningPage';
 import { api } from '../shared/api/api';
-import { PageInfo } from '../shared/components/PageInfo/PageInfo';
 
 export const Route = createFileRoute('/server-warning')({
   loader: async () => {
-    const response = await api.appInfo.callbackFn({ params: undefined });
+    const response = await api.appInfo.callbackFn({});
     return response.data.server_state;
   },
   component: ServerWarningPage,
 });
-
-function ServerWarningPage() {
-  const serverState = Route.useLoaderData();
-
-  const title =
-    serverState === 'setup'
-      ? m.server_warning_setup_title()
-      : m.server_warning_disconnected_title();
-  const subtitle =
-    serverState === 'setup'
-      ? m.server_warning_setup_subtitle()
-      : m.server_warning_disconnected_subtitle();
-
-  return (
-    <PageInfo
-      icon="warning"
-      title={title}
-      subtitle={subtitle}
-      link="/"
-      linkText={m.server_warning_retry()}
-    />
-  );
-}
