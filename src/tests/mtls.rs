6c975322e17b4ff691e7f84b540a17b803118463
Maciej Wójcik <maciej@defguard.net>
2026-04-21 12:24:22 +0200

    review fixes

--- src/tests/mtls.rs
+++ src/tests/mtls.rs
@@ -211,7 +211,7 @@ async fn call_bidi(client: &mut ProxyClient<Channel>) -> Status {
 async fn run_errors_without_tls_config() {
     let server = build_proxy_server();
     // configure() is deliberately NOT called.
-    let addr: SocketAddr = "127.0.0.1:0".parse().unwrap();
+    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 0);
     let result = server
         .run(addr, futures_util::future::pending::<()>())
         .await;
