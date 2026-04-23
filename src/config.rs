bb79647fb581587d5a037f7bfc3462ba7317e2ac
Maciej Wójcik <maciej@defguard.net>
2026-04-23 22:05:22 +0200

    set more permissive default rate limits

--- src/config.rs
+++ src/config.rs
@@ -29,11 +29,11 @@ fn default_adoption_timeout() -> u64 {
 }
 
 fn default_rate_limit_per_second() -> u64 {
-    10
+    100
 }
 
 fn default_rate_limit_burst() -> u32 {
-    100
+    1000
 }
 
 #[derive(Parser, Debug, Deserialize, Clone)]
@@ -58,11 +58,15 @@ pub struct EnvConfig {
     #[serde(default = "default_log_level")]
     pub log_level: LevelFilter,
 
-    #[arg(long, env = "DEFGUARD_PROXY_RATELIMIT_PERSECOND", default_value_t = 10)]
+    #[arg(
+        long,
+        env = "DEFGUARD_PROXY_RATELIMIT_PERSECOND",
+        default_value_t = 100
+    )]
     #[serde(default = "default_rate_limit_per_second")]
     pub rate_limit_per_second: u64,
 
-    #[arg(long, env = "DEFGUARD_PROXY_RATELIMIT_BURST", default_value_t = 100)]
+    #[arg(long, env = "DEFGUARD_PROXY_RATELIMIT_BURST", default_value_t = 1000)]
     #[serde(default = "default_rate_limit_burst")]
     pub rate_limit_burst: u32,
 
