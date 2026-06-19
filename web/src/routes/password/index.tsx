71e6dbf5ad7050ef4a1a35b70bc752f7a9b99527
Maciej Wójcik <maciej@defguard.net>
2026-06-19 11:25:20 +0200

    hide reset password option

--- web/src/routes/password/index.tsx
+++ web/src/routes/password/index.tsx
@@ -1,6 +1,17 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, redirect } from '@tanstack/react-router';
+import { queryClient } from '../../app/query';
 import { PasswordStartPage } from '../../pages/PasswordStart/PasswordStartPage';
+import { getAppInfoQueryOptions } from '../../shared/query/queryOptions';
 
 export const Route = createFileRoute('/password/')({
   component: PasswordStartPage,
+  loader: async () => {
+    const appInfo = await queryClient.ensureQueryData(getAppInfoQueryOptions);
+    if (!appInfo.data.display_password_reset) {
+      throw redirect({
+        to: '/',
+        replace: true,
+      });
+    }
+  },
 });
