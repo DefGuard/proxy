af3f1041a30b943729d311866388fc52cb2cb1f9
Maciej Wójcik <maciej@defguard.net>
2026-09-22 12:11:58 +0200

    update nix shell

--- flake.nix
+++ flake.nix
@@ -43,7 +43,9 @@
 
       # define shared build inputs
       nativeBuildInputs = with pkgs; [rustToolchain pkg-config];
-      buildInputs = with pkgs; [openssl protobuf nodejs_26 pnpm_11];
+      buildInputs = with pkgs;
+        [openssl protobuf nodejs_26 pnpm_11]
+        ++ lib.optionals stdenv.hostPlatform.isLinux [systemd];
     in {
       devShells.default = pkgs.mkShell {
         inherit nativeBuildInputs buildInputs;
